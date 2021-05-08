f045e1705073d0f7a7cd7cd00169ca34305df18c
Michelle Naffate <michelle.naffate205164@potros.itson.edu.mx>
2021-05-08 03:32:47 -0700

    Permite registrar, iniciar, sesion y recuperacion de contraseña

--- app/src/main/java/com/example/digimind/ContrasenaActivity.kt
+++ app/src/main/java/com/example/digimind/ContrasenaActivity.kt
@@ -1,16 +1,24 @@
 package com.example.digimind
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class ContrasenaActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contrasena)
+
+        auth = Firebase.auth
 
         val btn_restablecer: Button = findViewById(R.id.btn_restablecer)
 
@@ -20,7 +28,18 @@ class ContrasenaActivity : AppCompatActivity() {
             var correo: String = et_correo.text.toString()
 
             if(!correo.isNullOrBlank()){
-                //enviarcorreo
+
+                auth.sendPasswordResetEmail(correo)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful){
+                            Toast.makeText(this,"Se envio un correo a $correo", Toast.LENGTH_SHORT).show()
+                            val intent: Intent = Intent(this, LoginActivity::class.java)
+                            startActivity(intent)
+                        }else{
+                            Toast.makeText(this,"Error al enviar correo", Toast.LENGTH_SHORT).show()
+                        }
+
+                    }
             }else{
                 Toast.makeText(this,"Ingresar correo",
                     Toast.LENGTH_SHORT).show()
