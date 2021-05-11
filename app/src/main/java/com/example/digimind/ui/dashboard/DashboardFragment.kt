9ac31ba425c32ca53a186d669e47d8b36d3c065c
Michelle Naffate <michelle.naffate205164@potros.itson.edu.mx>
2021-05-11 01:53:47 -0700

    nuevos cambios ya se guardan los taks y se pueden ver en la pantalla homeFragment

--- app/src/main/java/com/example/digimind/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/digimind/ui/dashboard/DashboardFragment.kt
@@ -1,6 +1,7 @@
 package com.example.digimind.ui.dashboard
 
 import android.app.TimePickerDialog
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,26 +11,43 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.digimind.R
+import com.example.digimind.RegistroActivity
 import com.example.digimind.Task
 import com.example.digimind.ui.home.HomeFragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.ktx.storage
+import kotlin.collections.HashMap
+
 
 class DashboardFragment : Fragment() {
 
+    private lateinit var storage:FirebaseFirestore
+    private lateinit var auth: FirebaseAuth
     private lateinit var dashboardViewModel: DashboardViewModel
+
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
+
+
     ): View? {
         dashboardViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel::class.java)
+            ViewModelProviders.of(this).get(DashboardViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-
+        storage= FirebaseFirestore.getInstance()
+        auth= FirebaseAuth.getInstance()
         val btn_time: Button = root.findViewById(R.id.btn_time)
+
 
 
         btn_time.setOnClickListener{
@@ -59,34 +77,36 @@ class DashboardFragment : Fragment() {
 
             var titulo = et_titulo.text.toString()
             var time = btn_time.text.toString()
+            val map: HashMap<String, Any> = hashMapOf("email" to auth.currentUser.email,
+                "actividad" to "$titulo",
+                "tiempo" to "$time",
+                "lu" to checkMonday.isChecked,
+                "ma" to checkTuesday.isChecked,
+                "mi" to checkWednesday.isChecked,
+                "ju" to checkThursday.isChecked,
+                "vi" to checkFriday.isChecked,
+                "sa" to checkSaturday.isChecked,
+                "do" to checkSunday.isChecked)
 
-            var days = ArrayList<String>()
 
-            if(checkMonday.isChecked)
-                days.add("Monday")
-            if(checkTuesday.isChecked)
-                days.add("Tuesday")
-            if(checkWednesday.isChecked)
-                days.add("Wednesday")
-            if(checkThursday.isChecked)
-                days.add("Thursday")
-            if(checkFriday.isChecked)
-                days.add("Friday")
-            if(checkSaturday.isChecked)
-                days.add("Saturday")
-            if(checkSunday.isChecked)
-                days.add("Sunday")
+            storage.collection("actividades")
+                .add(map)
+                .addOnSuccessListener {
+                    Toast.makeText(root.context, "Task Agregado", Toast.LENGTH_SHORT).show()
 
-            var task = Task(titulo, days, time)
-
-            HomeFragment.tasks.add(task)
-
-            Toast.makeText(root.context, "new task added", Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener{
+                    Toast.makeText(root.context, "Error: intente de nuevo", Toast.LENGTH_SHORT).show()
+                }
         }
 
 
         return root
     }
+
+
+
+
 
 
 }
--- app/src/main/java/com/example/digimind/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/digimind/ui/home/HomeFragment.kt
@@ -14,11 +14,17 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.digimind.R
 import com.example.digimind.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.fragment_home.*
+
 
 class HomeFragment : Fragment() {
 
     private var adaptador: AdaptadorTareas? = null
     private lateinit var homeViewModel: HomeViewModel
+    private lateinit var storage: FirebaseFirestore
+    private lateinit var auth: FirebaseAuth
 
     companion object{
         var tasks = ArrayList<Task>()
@@ -31,51 +37,90 @@ class HomeFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
+            ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
 
-        if (first){
-            fillTasks()
-            first = false
+        tasks=ArrayList()
+
+        storage= FirebaseFirestore.getInstance()
+        auth= FirebaseAuth.getInstance()
+
+        fillTasks()
+
+
+        if(!tasks.isEmpty()) {
+            var gridView: GridView = root.findViewById(R.id.gridview)
+
+            adaptador = AdaptadorTareas(root.context, tasks)
+            gridView.adapter = adaptador
         }
-
-
-        Toast.makeText(root.context, tasks[0].toString(), Toast.LENGTH_LONG).show()
-        var gridView: GridView = root.findViewById(R.id.gridview)
-
-        adaptador = AdaptadorTareas(root.context, tasks)
-        gridView.adapter = adaptador
-
 
         return root
     }
 
     fun fillTasks(){
-        tasks.add(Task("Practice 1", arrayListOf("Tuesday"), "17:30"))
-        tasks.add(Task("Practice 2", arrayListOf("Monday", "Sunday"), "17:40"))
-        tasks.add(Task("Practice 3", arrayListOf("Wednesday"), "14:00"))
-        tasks.add(Task("Practice 4", arrayListOf("Saturday"), "11:00"))
-        tasks.add(Task("Practice 5", arrayListOf("Friday"), "13:00"))
-        tasks.add(Task("Practice 6", arrayListOf("Thursday"), "10:40"))
-        tasks.add(Task("Practice 7", arrayListOf("Monday"), "12:00"))
 
+        storage.collection("actividades")
+            .whereEqualTo("email",auth.currentUser.email)
+            .get()
+            .addOnSuccessListener {
+                it.forEach {
+                    var dias=ArrayList<String>()
+
+                    if(it.getBoolean("lu") == true){
+                        dias.add("Monday")
+                    }
+
+                    if(it.getBoolean("ma") == true){
+                        dias.add("Tuesday")
+                    }
+
+                    if(it.getBoolean("mi") == true){
+                        dias.add("Wednesday")
+                    }
+
+                    if(it.getBoolean("ju") == true){
+                        dias.add("Thursday")
+                    }
+
+                    if(it.getBoolean("vi") == true){
+                        dias.add("Friday")
+                    }
+
+                    if(it.getBoolean("sa") == true){
+                        dias.add("Saturday")
+                    }
+
+                    if(it.getBoolean("do") == true){
+                        dias.add("Sunday")
+                    }
+
+                    tasks!!.add(Task(it.getString("actividad")!!, dias, it.getString("tiempo")!!))
+                }
+
+                adaptador = AdaptadorTareas(context!!, tasks)
+                gridview.adapter=adaptador
+            }
+            .addOnFailureListener{
+                Toast.makeText(context, "Error: intente de nuevo", Toast.LENGTH_SHORT).show()
+            }
     }
 
     private class AdaptadorTareas: BaseAdapter {
-        var tasks = ArrayList<Task>()
+        var tareas = ArrayList<Task>()
         var context: Context? = null
 
-        constructor(contexto: Context, tasks: ArrayList<Task>){
+        constructor(contexto: Context, tareas: ArrayList<Task>){
             this.context = contexto
-            this.tasks = tasks
+            this.tareas = tareas
         }
 
         override fun getCount(): Int {
-            return tasks.size
+            return tareas.size
         }
 
         override fun getItem(p0: Int): Any {
-            return tasks[p0]
+            return tareas[p0]
         }
 
         override fun getItemId(p0: Int): Long {
@@ -83,7 +128,7 @@ class HomeFragment : Fragment() {
         }
 
         override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
-            var task = tasks[p0]
+            var task = tareas[p0]
             var inflador = LayoutInflater.from(this.context)
             var vista = inflador.inflate(R.layout.task_view, null)
 
